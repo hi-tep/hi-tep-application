8b5707c6463fff6ff4811bd5b739e7b8e61ef786
Thomas Baier <tbaier@math.bme.hu>
2025-02-03 17:06:05 +0100

    Remove test code

--- app/src/hitep_service/rest/test/openapi/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi.models.bounding_box import BoundingBox
-from openapi.models.coordinate import Coordinate
-from openapi.models.get_scenario200_response import GetScenario200Response
-from openapi.models.scenario_context import ScenarioContext
-from openapi.models.submit_gaze_request import SubmitGazeRequest
